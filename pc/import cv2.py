44d7bb7a8df833b04eccf61e5c62c2540458b749
flavioforenza <flavioforenza@192.168.1.52>
2021-11-02 17:37:02 +0100

    reset date folder

--- pc/import cv2.py
+++ pc/import cv2.py
@@ -1,15 +1,38 @@
-import cv2 
+import numpy as np
+import argparse
+import cv2
 
-frameWidth = 1280
-frameHeight = 720
-cap = cv2.VideoCapture(0)
-cap.set(3, frameWidth)
-cap.set(4, frameHeight)
-cap.set(10,150)
+#Next, add the parser command lines:
 
-while cap.isOpened():
-    success, img = cap.read()
-    if success:
-        cv2.imshow("Result", img)
-        if cv2.waitKey(1) & 0xFF == ord('q'):
-            break
+# construct the argument parse 
+parser = argparse.ArgumentParser(
+    description='Script to run MobileNet-SSD object detection network ')
+parser.add_argument("--video", help="path to video file. If empty, camera's stream will be used")
+parser.add_argument("--prototxt", default="MobileNetSSD_deploy.prototxt",
+                                  help='Path to text network file: '
+                                       'MobileNetSSD_deploy.prototxt for Caffe model or '
+                                       )
+parser.add_argument("--weights", default="MobileNetSSD_deploy.caffemodel",
+                                 help='Path to weights: '
+                                      'MobileNetSSD_deploy.caffemodel for Caffe model or '
+                                      )
+parser.add_argument("--thr", default=0.2, type=float, help="confidence threshold to filter out weak detections")
+args = parser.parse_args()
+
+
+
+
+
+# frameWidth = 1280
+# frameHeight = 720
+# cap = cv2.VideoCapture(0)
+# cap.set(3, frameWidth)
+# cap.set(4, frameHeight)
+# cap.set(10,150)
+
+# while cap.isOpened():
+#     success, img = cap.read()
+#     if success:
+#         cv2.imshow("Result", img)
+#         if cv2.waitKey(1) & 0xFF == ord('q'):
+#             break
