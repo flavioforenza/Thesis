017c71d29ff1e2ce89ba1c4aa8555c94ffc068e1
flavioforenza <flavioforenza@192.168.1.52>
2021-11-03 12:04:23 +0100

    start bench pc

--- pc/import cv2.py
+++ pc/import cv2.py
@@ -1,38 +1,146 @@
 import numpy as np
 import argparse
 import cv2
+import imutils
+import onnx
+import time
 
-#Next, add the parser command lines:
+from imutils.video import FPS
 
-# construct the argument parse 
-parser = argparse.ArgumentParser(
-    description='Script to run MobileNet-SSD object detection network ')
-parser.add_argument("--video", help="path to video file. If empty, camera's stream will be used")
-parser.add_argument("--prototxt", default="MobileNetSSD_deploy.prototxt",
-                                  help='Path to text network file: '
-                                       'MobileNetSSD_deploy.prototxt for Caffe model or '
-                                       )
-parser.add_argument("--weights", default="MobileNetSSD_deploy.caffemodel",
-                                 help='Path to weights: '
-                                      'MobileNetSSD_deploy.caffemodel for Caffe model or '
-                                      )
-parser.add_argument("--thr", default=0.2, type=float, help="confidence threshold to filter out weak detections")
-args = parser.parse_args()
+path_to_onnx = "data/networks/FCN-ResNet18-Cityscapes-512x256/fcn_resnet18.onnx"
+
+def get_fps(network_file, weights, input_lst, classNames):
+    for single_input in input_lst:
+        #single_input = ""
+        # construct the argument parse 
+        parser = argparse.ArgumentParser(description='Script to run MobileNet-SSD object detection network ')
+
+        parser.add_argument("--video", type=str, default=single_input, help="path to video file. If empty, camera's stream will be used")
+        parser.add_argument("--prototxt", type=str, default=network_file, help='Path to text network file: ''MobileNetSSD_deploy.prototxt for Caffe model or ')
+        parser.add_argument("--weights", type=str, default=weights, help='Path to weights: ''MobileNetSSD_deploy.caffemodel for Caffe model or ')
+        parser.add_argument("--thr", default=0.5, type=float, help="confidence threshold to filter out weak detections")
+        args = parser.parse_args()
+
+        # Next, open the video file or capture device depending what we choose, also load the model Caffe model.
+        # Open video file or capture device. 
+        if args.video != "":
+            cap = cv2.VideoCapture(args.video)
+            fps = FPS().start()
+        else:
+            frameWidth = 1280
+            frameHeight = 720
+            cap = cv2.VideoCapture(0)
+            cap.set(3, frameWidth)
+            cap.set(4, frameHeight)
+            cap.set(10,150)
+            fps = FPS().start()
+
+        #Load the Caffe model 
+        #net = cv2.dnn.readNetFromCaffe(args.prototxt, args.weights)
+        net_onnx = cv2.dnn.readNetFromONNX(path_to_onnx)
+
+        timeuout = time.time() + 10
 
 
+        while time.time()<=timeuout:
+            # Capture frame-by-frame
+            ret, frame = cap.read()
+            frame_resized = cv2.resize(frame,(100,100))
+            blob = cv2.dnn.blobFromImage(frame, 1.0 / 255, (224, 224),(0, 0, 0), swapRB=True, crop=False)
+            #net.setInput(blob)
+            #detections = net.forward()
 
+            net_onnx.setInput(blob)
+            detections_onnx = net_onnx.forward()
 
+            #Size of frame resize (300x300)
+            cols = frame_resized.shape[1] 
+            rows = frame_resized.shape[0]
 
-# frameWidth = 1280
-# frameHeight = 720
-# cap = cv2.VideoCapture(0)
-# cap.set(3, frameWidth)
-# cap.set(4, frameHeight)
-# cap.set(10,150)
+            #For get the class and location of object detected, 
+            # There is a fix index for class, location and confidence
+            # value in @detections array .
+            for i in range(detections_onnx.shape[2]):
+                confidence = detections_onnx[0, 0, i, 2] #Confidence of prediction
+                #confidence = confidence*-1 
+                if confidence > args.thr: # Filter prediction 
+                    class_id = int(detections_onnx[0, 0, i, 1]) # Class label
+                    # Object location 
+                    xLeftBottom = int(detections_onnx[0, 0, i, 3] * cols) 
+                    yLeftBottom = int(detections_onnx[0, 0, i, 4] * rows)
+                    xRightTop   = int(detections_onnx[0, 0, i, 5] * cols)
+                    yRightTop   = int(detections_onnx[0, 0, i, 6] * rows)
+            
+                    # Factor for scale to original size of frame
+                    heightFactor = frame.shape[0]/300.0  
+                    widthFactor = frame.shape[1]/300.0 
+                    # Scale object detection to frame
+                    xLeftBottom = int(widthFactor * xLeftBottom) 
+                    yLeftBottom = int(heightFactor * yLeftBottom)
+                    xRightTop   = int(widthFactor * xRightTop)
+                    yRightTop   = int(heightFactor * yRightTop)
+                    # Draw location of object  
+                    cv2.rectangle(frame, (xLeftBottom, yLeftBottom), (xRightTop, yRightTop),
+                                (0, 255, 0))
 
-# while cap.isOpened():
-#     success, img = cap.read()
-#     if success:
-#         cv2.imshow("Result", img)
-#         if cv2.waitKey(1) & 0xFF == ord('q'):
-#             break
+                    # Draw label and confidence of prediction in frame resized
+                    if class_id in classNames:
+                        label = classNames[class_id] + ": " + str(confidence)
+                        labelSize, baseLine = cv2.getTextSize(label, cv2.FONT_HERSHEY_SIMPLEX, 0.5, 1)
+
+                        yLeftBottom = max(yLeftBottom, labelSize[1])
+                        cv2.rectangle(frame, (xLeftBottom, yLeftBottom - labelSize[1]),
+                                            (xLeftBottom + labelSize[0], yLeftBottom + baseLine),
+                                            (255, 255, 255), cv2.FILLED)
+                        cv2.putText(frame, label, (xLeftBottom, yLeftBottom),
+                                    cv2.FONT_HERSHEY_SIMPLEX, 0.5, (0, 0, 0))
+
+                        
+                        print(label)
+
+                cv2.namedWindow("frame", cv2.WINDOW_NORMAL)
+                cv2.imshow("frame", frame)
+                if cv2.waitKey(1) >= 0:  # Break with ESC 
+                    break
+            fps.update()
+        fps.stop()
+        print("[INFO] elasped time: {:.2f}".format(fps.elapsed()))
+        print("[INFO] approx. FPS: {:.2f}".format(fps.fps()))
+        cap.release()
+        cv2.destroyAllWindows()
+        
+
+input_list = [
+	"data/video/road.mp4"
+			]
+
+#lables of network
+classNames = {
+    0:'void',
+    1:'ego_vehicle',
+    2:'ground',
+    3:'road',
+    4:'sidewalk',
+    5:'building',
+    6:'wall',
+    7:'fence',
+    8:'pole',
+    9:'traffic_light',
+    10:'traffic_sign',
+    11:'vegetation',
+    12:'terrain',
+    13:'sky',
+    14:'person',
+    15:'car',
+    16:'truck',
+    17:'bus',
+    18:'train',
+    19:'motorcycle',
+    20:'bicycle'
+}
+
+netwotrk = "data/networks/ped-100/deploy.prototxt"
+
+weights = "data/networks/ped-100/snapshot_iter_70800.caffemodel"
+
+get_fps(netwotrk,weights, input_list, classNames)
