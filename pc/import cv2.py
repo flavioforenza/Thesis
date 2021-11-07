e37467d0555616f01a78fd5158fea7db092acd31
flavioforenza <flavioforenza@192.168.1.50>
2021-11-07 15:54:12 +0100

    continue bench pc

--- pc/import cv2.py
+++ pc/import cv2.py
@@ -1,94 +1,99 @@
 import numpy as np
-import argparse
 import cv2
-import imutils
 import time
 import os 
+import pandas as pd
 
 from imutils.video import FPS
 
-def get_fps(path_to_onnx, input_lst):
-    for single_input in input_lst:
-        single_input = ""
-        # construct the argument parse 
-        parser = argparse.ArgumentParser(description='Script to run MobileNet-SSD object detection network')
+def get_fps(network, path_to_onnx, single_input):
+    path_network = 'data/networks/' + network
+    single_input = "data/" + single_input
+    if ".mp4" in single_input: #video.mp4
+        cap = cv2.VideoCapture(single_input)
+        total_num_frames = int(cap.get(cv2.CAP_PROP_FRAME_COUNT))
+        print("Total video's frames: ", total_num_frames)
+        (major_ver, _, _) = (cv2.__version__).split('.')
+        if int(major_ver)  < 3 :
+            fps_input = cap.get(cv2.cv.CV_CAP_PROP_FPS)
+        else:
+            fps_input = cap.get(cv2.CAP_PROP_FPS)
+    else: #webcam
+        frameWidth = 1280
+        frameHeight = 720
+        cap = cv2.VideoCapture(single_input)
+        cap.set(3, frameWidth)
+        cap.set(4, frameHeight)
+        cap.set(10,150)
+        total_num_frames = 120;
+        fps_input = 30   
+    
+    #net = cv2.dnn.readNetFromCaffe(args.prototxt, args.weights)
+    net_onnx = cv2.dnn.readNetFromONNX(path_network+'/'+path_to_onnx)
+    
+    timeuout = time.time() + 10
+    fps_start_time = 0
+    fps = 0
+    lst_frame = []
+    frame_count = 0
 
-        parser.add_argument("--video", type=str, default=single_input, help="path to video file. If empty, camera's stream will be used")
-        #parser.add_argument("--prototxt", type=str, default=network_file, help='Path to text network file: ''MobileNetSSD_deploy.prototxt for Caffe model or ')
-        #parser.add_argument("--weights", type=str, default=weights, help='Path to weights: ''MobileNetSSD_deploy.caffemodel for Caffe model or ')
-        parser.add_argument("--thr", default=0.5, type=float, help="confidence threshold to filter out weak detections")
-        args = parser.parse_args()
+    while time.time()<timeuout:
+        _, frame = cap.read()
+        frame_count +=1
 
-        if args.video != "": #video.mp4
-            cap = cv2.VideoCapture(args.video)
-            total_num_frames = int(cap.get(cv2.CAP_PROP_FRAME_COUNT))
-            print("Total video's frames: ", total_num_frames)
-            (major_ver, _, _) = (cv2.__version__).split('.')
-            if int(major_ver)  < 3 :
-                fps = cap.get(cv2.cv.CV_CAP_PROP_FPS)
-            else :
-                fps = cap.get(cv2.CAP_PROP_FPS)
-            print("FPS INFPUT:{0}".format(fps))
-        else: #webcam
-            frameWidth = 1280
-            frameHeight = 720
-            cap = cv2.VideoCapture(1)
-            total_num_frames = 120;
-            fps_web = 30   
-        
-        #net = cv2.dnn.readNetFromCaffe(args.prototxt, args.weights)
-        net_onnx = cv2.dnn.readNetFromONNX(path_to_onnx)
-        
-        timeuout = time.time() + 10
-        fps_start_time = 0
-        fps = 0
-        lst_frame = []
-        frame_count = 0
+        fps_end_time = time.time()
+        time_diff = fps_end_time - fps_start_time
+        fps = 1/time_diff
+        lst_frame.append(fps)
+        fps_start_time = fps_end_time
 
-        while time.time()<timeuout:
-            _, frame = cap.read()
-            frame_count +=1
+        (h, w) = frame.shape[:2]
+        blob = cv2.dnn.blobFromImage(frame, 0.007843, (h, w), 127.5)
 
-            fps_end_time = time.time()
-            time_diff = fps_end_time - fps_start_time
-            fps = 1/time_diff
-            lst_frame.append(fps)
-            fps_start_time = fps_end_time
+        net_onnx.setInput(blob)     
 
-            (h, w) = frame.shape[:2]
-            blob = cv2.dnn.blobFromImage(frame, 0.007843, (h, w), 127.5)
+        detections = net_onnx.forward()
 
-            net_onnx.setInput(blob)     
+        #start fps inference count 
+        fps_inference = FPS().start()
+        obj = 0
 
-            detections = net_onnx.forward()
+        for i in np.arange(0, detections.shape[2]):
+            confidence = detections[0, 0, i, 2]
+            if confidence > 0.5:
+                #do something 
+                obj +=1    
+                
+        fps_inference.update()#fps INFERENCE    
+        cv2.putText(frame, "FPS: {:.2f}".format(fps), (5,30), cv2.FONT_HERSHEY_PLAIN, 3,(0,255,0), 2)#FPS OUTPUT
+        cv2.imshow("Frame", frame)
+        key = cv2.waitKey(1)
+        if key==81 or key ==113:
+            break
 
-            #start fps inference count 
-            fps_inference = FPS().start()
-            obj = 0
+    fps_inference.stop()
+    print("FPS INPUT: {0}".format(fps_input))
+    print("FPS OUTPUT: ", max(lst_frame))
+    print("FPS INFERENCE: {:.2f}".format(fps_inference.fps()))
 
-            for i in np.arange(0, detections.shape[2]):
-                confidence = detections[0, 0, i, 2]
-                if confidence > args.thr:
-                    #do something 
-                    obj +=1    
-                    
-            fps_inference.update()#fps INFERENCE    
-            cv2.putText(frame, "FPS: {:.2f}".format(fps), (5,30), cv2.FONT_HERSHEY_PLAIN, 3,(0,255,0), 2)#FPS OUTPUT
-            cv2.imshow("Frame", frame)
-            key = cv2.waitKey(1)
-            if key==81 or key ==113:
-                break
+    cap.release()
+    cv2.destroyAllWindows()
 
-        fps_inference.stop()
-        print("FPS INFERENCE: {:.2f}".format(fps_inference.fps()))
-        print("FPS OUTPUT: ", max(lst_frame))
-        cap.release()
-        cv2.destroyAllWindows()
-        
+    return fps_input, max(lst_frame), fps_inference.fps()
 
-input_list = [
-	"data/video/4k_60485=fps.mp4"
-			]
+lst_input = [
+	"video/240p_60fps.mp4",
+	"video/360p_30fps.mp4",
+	"video/480p_30fps.mp4",
+	"video/720p_30fps.mp4",
+	"video/1080p_30fps.mp4",
+	"video/1080p_60fps.mp4",
+	0, 
+	1
+    ]
+
+lst_networks = []
+lst_path_onnx = []
 
 list_accepted_folders = ["ResNet18-Cityscapes", "ResNet18-Pascal-VOC"]
 dir_path = [path for path in os.walk("data/networks/")]
@@ -99,4 +104,19 @@ for i in range(1, len(dir_path)): #start from first
             #get the onnx file
             for file in current_path[2]:
                 if '.onnx' in file and '.engine' not in file:
-                    get_fps(current_path[0]+"/"+file, input_list)
+                    lst_networks.append(current_path[0].replace('data/networks/',""))
+                    lst_path_onnx.append(file)
+                   
+results_dict = {}
+for i in range (0, len(lst_networks)):
+    for input_source in lst_input:
+        input, output , network_inference = get_fps(lst_networks[i], lst_path_onnx[i], input_source)
+        results_dict[(lst_networks[i],input_source)] = [input, output, network_inference]
+    
+
+
+
+
+#dataframe = pd.DataFrame(columns=["Input", "Output", "Network"], index=lst_netowrks)
+
+                    
