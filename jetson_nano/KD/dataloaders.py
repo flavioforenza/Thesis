3f3df986ab272ac792820b9a366c5fcebe00a5ad
flavioforenza <flavio.forenza@studenti.unimi.it>
2022-01-24 18:20:55 +0100

    train teacher model

---
+++ jetson_nano/KD/dataloaders.py
@@ -0,0 +1,39 @@
+import glob
+import cv2
+import numpy as np
+import torch
+from torch.utils.data import Dataset, DataLoader
+
+class CustomDataset(Dataset):
+    def __init__(self):
+        self.imgs_path = "data/train/"
+        file_list = glob.glob(self.imgs_path + "*")
+        print(file_list)
+        self.data = []
+        for class_path in file_list:
+            class_name = class_path.split("/")[-1]
+            for img_path in glob.glob(class_path + "/*.jpg"):
+                self.data.append([img_path, class_name])
+        print(self.data)
+        self.class_map = {"bicycle" : 0, 
+                        "bus": 1, 
+                        "car": 2, 
+                        "motorcycle": 3, 
+                        "person": 4, 
+                        "traffic_light": 5, 
+                        "traffic_sign": 6,
+                        "truck": 7 }
+        self.img_dim = (300, 300)
+
+    def __len__(self):
+        return len(self.data)
+
+    def __getitem__(self, idx):
+        img_path, class_name = self.data[idx]
+        img = cv2.imread(img_path)
+        img = cv2.resize(img, self.img_dim)
+        class_id = self.class_map[class_name]
+        img_tensor = torch.from_numpy(img)
+        img_tensor = img_tensor.permute(2, 0, 1)
+        class_id = torch.tensor([class_id])
+        return img_tensor, class_id
---
+++ jetson_nano/KD/evaluate.py
@@ -0,0 +1,89 @@
+import matplotlib.pyplot as plt
+import numpy as np
+import models
+import torch.utils.data
+import torchvision
+import os
+
+BASE_DIR = './data'
+
+def evaluate(model_type='teacher', prefix='teacher'):
+    if model_type == 'teacher':
+        model = models.Teacher()
+    elif model_type == 'student':
+        model = models.Student()
+       
+    model_paths = []
+    for epoch_count in range(0, 3000, 100):
+        model_paths.append(os.path.join(BASE_DIR, '{}-{}.pth'.format(prefix, epoch_count)))
+    model_paths.append(os.path.join(BASE_DIR, '{}.pth'.format(prefix)))
+    
+    # Get test data
+    dataset = torchvision.datasets.MNIST('../mnist', train=False, download=True,
+                                                    transform=torchvision.transforms.Compose([
+                                                        torchvision.transforms.ToTensor(), 
+                                                        torchvision.transforms.Normalize((0.1307,), (0.3081,))
+                                                        ]))
+
+    # Create data loader
+    batch_size = 10000
+    data_loader = torch.utils.data.DataLoader(dataset, batch_size=batch_size, shuffle=False)
+
+    error_counts = np.zeros(shape=len(model_paths), dtype=np.int32)
+    for model_count, model_path in enumerate(model_paths):
+        model.load_state_dict(torch.load(model_path))
+        model.eval()
+        model = model.cuda()
+
+        error_count = 0
+        for x, y_gt in data_loader:
+            # Preprocess input
+            x = x.flatten(start_dim=1, end_dim=-1)
+            x = x.cuda()
+            y_gt = y_gt.cuda()
+
+            # Predict label
+            y_pred = model(x)
+            with torch.no_grad():
+                y_pred = torch.argmax(y_pred, dim=1)
+                error_count += batch_size - torch.eq(y_pred, y_gt).sum()
+        
+        error_counts[model_count] = error_count
+        print('{}-{}: {}'.format(prefix, model_count, error_count))
+
+    return error_counts
+
+# Evaluate accuracy
+# error_counts_teacher = evaluate('teacher', 'teacher')
+# error_counts_student = evaluate('student', 'student')
+# error_counts_student_distill = evaluate('student', 'student-distill')
+# error_counts_teacher_distill = evaluate('teacher', 'teacher-distill')
+
+# Store as file
+# np.save('./data/error_counts_teacher.npy', error_counts_teacher)
+# np.save('./data/error_counts_student.npy', error_counts_student)
+# np.save('./data/error_counts_student_distill.npy', error_counts_student_distill)
+# np.save('./data/error_counts_teacher_distill.npy', error_counts_teacher_distill)
+
+# # Load from file
+error_counts_teacher = np.load('./data/error_counts_teacher.npy')
+error_counts_student = np.load('./data/error_counts_student.npy')
+error_counts_student_distill = np.load('./data/error_counts_student_distill.npy')
+error_counts_teacher_distill = np.load('./data/error_counts_teacher_distill.npy')
+
+# Prepare to plot
+fig, ax = plt.subplots()
+
+# Plot error
+ax.plot(range(0, 3001, 100), error_counts_teacher, label='teacher')
+ax.plot(range(0, 3001, 100), error_counts_student, label='student')
+ax.plot(range(0, 3001, 100), error_counts_student_distill, label='student with distillation')
+ax.plot(range(0, 3001, 100), error_counts_teacher_distill, label='teacher with distillation')
+ax.set_xlabel('number of epochs')
+ax.set_ylabel('number of errors')
+ax.set_title('Learning curve')
+ax.legend()
+
+# Show
+#plt.show()
+plt.savefig('learning_curve.png')
